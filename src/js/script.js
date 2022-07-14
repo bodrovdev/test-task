9a851554dff38b493c4d500a7db310bad7d939c8
Igor Bodrov <bodrovigor02@gmail.com>
2022-07-14 15:29:01 +0300

    feat: 🎸 Исправли ошибки, добавил адаптивность

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
+let navButton = document.getElementById('nav-button');
+let menuButton = document.getElementById('nav-menu');
+let menuLogo = document.getElementById('nav-logo');
 
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('navigation-menu__burger--opened');
+  menuButton.classList.toggle('page-nav--opened');
+  menuLogo.classList.toggle('navigation-menu__logo-block--opened');
+})
